90c572905b1e0ba3aca0f25f539974d367b560a4
daod2003 <1253@holbertonschool.com>
2020-03-23 21:10:34 +0000

    change -f for only pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process named killmenow
 
-exec { 'pkill':
-  command  => 'pkill -f killmenow',
+exec { 'kill':
+  command  => 'pkill killmenow',
   provider => 'shell',
 }
