421c5ccd06e5e7327609677f6dd800c5a5c4710b
daod2003 <1253@holbertonschool.com>
2020-05-04 21:32:28 -0500

    add file 2-export_to_JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    idU = argv[1]
+    urlUser = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(idU)).json()
+    listTodo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'
+        .format(idU)).json()
+    pendingList = []
+    for pending in listTodo:
+        pendingDict = {}
+        pendingDict['pending'] = pending.get('title')
+        pendingDict['completed'] = pending.get('completed')
+        pendingDict['username'] = pending.get('username')
+        pendingList.append(pendingDict)
+    jObject = {}
+    jObject[idU] = pendingList
+    with open('{}.json'.format(idU), 'w') as jsonfile:
+        json.dump(jObject, jsonfile)
