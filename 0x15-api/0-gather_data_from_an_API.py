afc86d8bf085b4febb2d590a3fbc15f2bf54052c
daod2003 <1253@holbertonschool.com>
2020-05-04 20:43:33 -0500

    add print pendingTodoList in line 19 and change line 13

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,9 +10,10 @@ if __name__ == '__main__':
     listTodo = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idU)).json()
     pendingList = []
-    for pending in pendingList:
-        if pending.get('complete') is True:
+    for pending in listTodo:
+        if pending.get('completed') is True:
             pendingList.append(pending.get('title'))
     print('Employee {} is done with tasks({}/{}):'.format(urlUser.get('name'),
                                                           len(pendingList),
                                                           len(listTodo)))
+    print('\n'.join('\t {}'.format(pending) for pending in pendingList))
