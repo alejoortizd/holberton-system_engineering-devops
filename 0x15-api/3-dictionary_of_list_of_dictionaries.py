e7818e1aa2c6c7dc79acf2605745640217115fea
daod2003 <1253@holbertonschool.com>
2020-05-04 22:12:10 -0500

    add final file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+if __name__ == '__main__':
+    urlUser = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+    listTodo = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+
+    users = {}
+    usernames = {}
+    for user in users:
+        idU = user.get('id')
+        users[idU] = []
+        usernames[idU] = user.get('username')
+
+    for pending in listTodo:
+        pendings = {}
+        idU = pending.get('userId')
+        pendings['task'] = pending.get('title')
+        pendings['completed'] = pending.get('completed')
+        pendings['username'] = usernames.get('idU')
+        users.get(idU).append(pendings)
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump(users, jsonfile)
