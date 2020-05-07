2672e7b59aafaae835e00d93eb31ca47c00a2944
daod2003 <1253@holbertonschool.com>
2020-05-07 13:33:26 -0500

    Change condition in line 12

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,17 +7,17 @@ import requests
 from sys import argv
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=''):
     """Function that returns the list of all hot posts"""
-    if subreddit is None or type(subreddit) is not str:
-        print(None)
+    if after is None:
+        return []
     headers = {"User-Agent":
                "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                 AppleWebKit/537.36 (KHTML, like Gecko)\
                 Chrome/70.0.3538.77 Safari/537.36"}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
-        url += '?after={}'.format(after)
+        url += '?limit=100&after={}'.format(after)
     req = requests.get(url, headers=headers, allow_redirects=False)
     if str(req) != '<Response [200]>':
         return None
