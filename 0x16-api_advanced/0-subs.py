aa342375262c7098056a6806df21cc992b596e48
daod2003 <1253@holbertonschool.com>
2020-05-07 11:43:23 -0500

    Add file 0-subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Return subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that returns all subscribes"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    headers = {"User-Agent":
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/70.0.3538.77 Safari/537.36"}
+    req = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                       headers=headers).json()
+    subs = req.get("data", {}).get("subscribers", 0)
+    return subs
