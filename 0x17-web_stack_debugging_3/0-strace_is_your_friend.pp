84e117af356ca1785f9994a07ec927051ff1a776
David Ortiz Duque <davidod@pop-os.localdomain>
2020-05-20 10:35:52 -0500

    modified line 4

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Apache is returning a 500 error
-exec { ' fix double pp':
-  command => "sed -i 's/phpp/php/' /vaar/www/html/wp-settings.php",
+# Fix wordpress
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/bin/',
 }
